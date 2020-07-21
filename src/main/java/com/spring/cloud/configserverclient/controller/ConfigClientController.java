6decfa7e4de056c1c725906afe4ad77f4d1d5a9e
senth542002 <senth542002@gmail.com>
2020-07-21 22:56:14 +0530

    Initial commit for client

---
+++ src/main/java/com/spring/cloud/configserverclient/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.configserverclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+	
+	@Value("${user.role}")
+	private String role;
+	
+	@GetMapping("/whoami/{username}")
+	public String whoAmI(@PathVariable("username") String username) {
+		
+		return String.format("Hello! You are %s and you will become a %s ...\n", username, role);
+	}
+
+}
