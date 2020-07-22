b5d630ebfc290e0d43aa110a4d080ed434d69926
senth542002 <senth542002@gmail.com>
2020-07-22 10:55:25 +0530

    commiting changes

--- src/main/java/com/spring/cloud/configserverclient/controller/ConfigClientController.java
+++ src/main/java/com/spring/cloud/configserverclient/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.spring.cloud.configserverclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ConfigClientController {
 	
